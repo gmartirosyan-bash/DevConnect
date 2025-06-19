f335a32512af1c9dfd7062ac6bf3d5026a31c0dd
GagoYaxsh <gagikmartirosyan753@gmail.com>
2025-06-19 10:35:22 +0400

    uibuild

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use('/api/cards', middleware.tokenExtractor, middleware.userExtractor, cards
 app.use('/api/login', loginRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
 })
 
 app.use(middleware.unknownEndpoint)
